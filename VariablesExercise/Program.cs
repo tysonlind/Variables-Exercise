eef33eb85dc2b16a110bc56e4bc68b19f7979f30
tyson lind <tysonfloydlind@gmail.com>
2023-01-24 13:47:03 -0700

    completed assignment, assigning variables of each requested type and interpolating them in a Console.WriteLine output

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace VariablesExercise
+﻿
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        string myString = "I made a string";
+        int x = 10;
+        char myChar = 'a';
+        bool isTasty = true;
+        double bigNum = 126.01;
+        decimal thisNum = 2.13m;
+
+        Console.WriteLine($"This is an interpolated string {myString} followed by this interpolated number {x}, then the character {myChar}, then the boolean {isTasty}, then the double {bigNum} and finally the decimal {thisNum}");
         }
+       
     }
 }
+
